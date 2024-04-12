033503c5583c6b66d31c473258f88a6570371714
Milutin-S <studen.m98@gmail.com>
2024-04-13 00:33:45 +0200

    Add initial GPTeller test.

--- tests/gpteller.py
+++ tests/gpteller.py
@@ -1,1 +1,17 @@
 import unittest
+from qteller.GPTeller import GPTeller
+
+
+class GPTellerTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.gpteller = GPTeller()
+
+    def test_complet(self):
+        self.assertEqual(
+            len(self.gpteller.complete(query="Hello Alien", num_return_sequences=5)), 5
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
