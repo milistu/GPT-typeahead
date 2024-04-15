b7734de5ee67ea19e7154ccdabcba9b8bc6cdcb9
Milutin-S <studen.m98@gmail.com>
2024-04-15 23:15:17 +0200

    Update test for num return sequences

--- tests/gpteller.py
+++ tests/gpteller.py
@@ -5,12 +5,17 @@ from qteller.GPTeller import GPTeller
 class GPTellerTest(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
-        self.gpteller = GPTeller()
-
-    def test_complet(self):
-        self.assertEqual(
-            len(self.gpteller.complete(query="Hello Alien", num_return_sequences=5)), 5
+        self.teller = GPTeller()
+        self.num_return_sequences = 3
+        self.compleations = self.teller.complete(
+            query="a red",
+            context="car dealership",
+            max_length=5,
+            num_return_sequences=self.num_return_sequences,
         )
+
+    def test_num_return_sequences(self):
+        self.assertEqual(len(self.compleations), self.num_return_sequences)
 
 
 if __name__ == "__main__":
